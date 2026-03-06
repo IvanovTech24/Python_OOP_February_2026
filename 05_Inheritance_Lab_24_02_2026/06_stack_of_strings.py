9d15f5893956fd8940b54dd877cfa349a84ab2ca
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-03-06 15:54:04 +0200

    feat: add Stack class with methods for push, pop, top, and checking if empty

---
+++ 05_Inheritance_Lab_24_02_2026/06_stack_of_strings.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.data: list = []
+
+    def push(self, element: str):
+        self.data.append(element)
+
+    def pop(self):
+        return self.data.pop()
+
+    def top(self):
+        return self.data[-1]
+
+    def is_empty(self):
+        return not any(self.data)
+
+    def __str__(self):
+        return "[" + ", ".join(reversed(self.data)) + "]"
+
+s = Stack()
+
+s.push("1")
+s.push("2")
+s.push("3")
+print(s)
+s.pop()
+print(s)
