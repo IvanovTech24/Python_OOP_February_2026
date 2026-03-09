0d4be42379c9b220ff6cb15e9b02daacb6d8e18d
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-03-09 15:29:08 +0200

    feat: add PhotoAlbum class with methods for photo management and display

---
+++ 10_Static_and_Class_Methods_Exercise_05_03_2026/01_photo_album.py
@@ -0,0 +1,43 @@
+from math import ceil
+
+
+class PhotoAlbum:
+    PAGE_SIZE = 4
+
+    def __init__(self, pages: int):
+        self.pages = pages
+        self.photos: list[list[str]] = [[] for _ in range(self.pages)]
+
+    @classmethod
+    def from_photos_count(cls, photos_count: int) -> "PhotoAlbum":
+        return cls(ceil(photos_count / cls.PAGE_SIZE))
+
+    def add_photo(self, label: str) -> str:
+        for i, page in enumerate(self.photos, 1):
+            if len(page) < PhotoAlbum.PAGE_SIZE:
+                page.append(label)
+                return (f"{label} photo added successfully "
+                        f"on page {i} slot {len(page)}")
+        return "No more free slots"
+
+    def display(self) -> str:
+        sep = "-" * 11 + "\n"
+        result = sep
+        for page in self.photos:
+            result += " ".join(["[]" for _ in page]) + "\n"
+            result += sep
+        return result.strip()
+
+
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
+
+print(album.display())
