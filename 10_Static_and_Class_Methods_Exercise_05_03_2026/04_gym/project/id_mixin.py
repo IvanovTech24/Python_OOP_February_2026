656e12563384f2a31cd802fe10c0edf039163372
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-03-10 09:31:35 +0200

    feat: add Customer, Equipment, ExercisePlan, Subscription, Trainer classes and Gym management for fitness application

---
+++ 10_Static_and_Class_Methods_Exercise_05_03_2026/04_gym/project/id_mixin.py
@@ -0,0 +1,10 @@
+class IdMixin:
+    id: int = 1
+
+    @classmethod
+    def get_next_id(cls) -> int:
+        return cls.id
+
+    @classmethod
+    def increment_id(cls) -> None:
+        cls.id += 1
