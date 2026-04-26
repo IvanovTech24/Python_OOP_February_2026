0079981b1249b99e98665a2baf7b55288f444179
Ivan Iliyanov Ivanov <ivanovsb71@gmail.com>
2026-04-26 15:20:31 +0300

    feat: implement command pattern with Invoker, Command, and Receiver classes

---
+++ 21_Design_Patterns_Lab_24_03_2026/command_from_presentation.py
@@ -0,0 +1,43 @@
+from abc import ABC, abstractmethod
+
+
+class Invoker:
+    def __init__(self):
+        self._commands = []
+
+    def store_command(self, command):
+        self._commands.append(command)
+
+    def execute_commands(self):
+        for command in self._commands:
+            command.execute()
+
+
+class Command(ABC):
+    def __init__(self, receiver):
+        self._receiver = receiver
+
+    @abstractmethod
+    def execute(self):
+        pass
+
+
+class ConcreteCommand(Command):
+    def execute(self):
+        self._receiver.action()
+
+
+class Receiver:
+    def action(self):
+        pass
+
+
+def main():
+    receiver = Receiver()
+    concrete_command = ConcreteCommand(receiver)
+    invoker = Invoker()
+    invoker.store_command(concrete_command)
+    invoker.execute_commands()
+
+if __name__ == "__main__":
+    main()
